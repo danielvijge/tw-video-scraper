8efb74260b8b19ffb5889ae04184d6eb1efed2c1
Daniel Vijge <daniel@vijge.net>
2011-12-09 22:18:28 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,140 @@
+# tw-video-scraper
+
+`tw-video-scraper` is a small python script that can retrieve fan art
+thumbnail for a video file. It is designed to run with TwonkyMedia server,
+but can possibly be of use to others as well.
+
+## Background
+
+[TwonkyMedia server](http://www.twonky.com) is a DLNA server that runs on a lot
+of different devices. Using `ffmpeg` Twonky can generate thumbnails for video
+files. A guide to setting up Twonky can be found
+[here](http://server.vijge.net/archive/twonky-dlna-with-video-thumbnails/)
+
+One of the problems of using `ffmpeg` is that it takes a snapshot of the video
+file at a fixed time, for example at 30 seconds. For both movies and series
+this can be a problem. If there are opening credits, the thumbnail will display
+that. This might not be a very meaningful thumbnail for the video file.
+
+For a lot of movies and series there is fan art on the internet. This is a
+thumbnail that is more meaningful for a specific serie's episode.
+`tw-video-scraper` tries to get the fan art thumbnail for a video file. If no
+thumbnail can be found, `ffmpeg` is used to generate a thumbnail.
+
+## How to use
+
+`tw-video-scraper` is a python script that takes two arguments. The first is
+the name of the video file, the second is the output image file.
+
+	python tw-video-scraper.py /path/to/cool.serie.s01e04.htdv.avi /images/abc.jpg
+
+To use it with Twonky, save `tw-video-scraper.py`, `python-tw-video-scraper.desc` 
+and `python.location` in Twonky's cgi-bin directory. Make all files executable
+
+	chmod 700 tw-video-scraper.py python-python-tw-video-scraper.desc python.location
+
+If needed, change the values in `python.location`, `tw-video-scraper.py`, and
+`python-tw-video-scraper.desc`.
+The file `ffmpeg-video-thumb.desc` is no longer needed. Either comment out all
+lines are delete the file.
+If you want to regenerate all thumbnail, delete everything from the cache folder
+in the twonky directory (i.e. `/var/twonkymedia/twonkymedia/db/cache`)
+Now restart Twonky, and the script is used.
+
+At least `jpeg-scale.desc` and `cgi-jpegscale` plugin must also be enabled.
+
+## How it works
+
+Thumbnail for series are retrieved from TheTVDB.com. For movies, TheMovieDB.org is
+used. For each file, the script tries to guess the name and year of the movie, or
+the name of the series, the season, and the episode number. This is done by
+regular expression patterns.
+
+### Series
+
+First, it tries to determine if the file is a series. It is a series if the file name
+matches one of the following conditions:
+
+- The file name containing S01E01 (or any one of two digit number)
+- The file name containing 1x01 (or any one of two digit number)
+- The file name containing 'season 1 - episode 01' (or any one of two digit number, hyphen can be omitted)
+
+Anything immediately before the S01E01 or 1x01 is treated as the serie's name. If the
+last character is a hyphen (-), this is removed first. Periods (.) and underscores (_)
+are replaced by space ( ) first. With this name, the script used the TheTVDB.com API to
+find the correct serie. A serie is only found if the name matches the serie name exactly.
+A semicolon (:) in the serie's name is removed. Names are compared case insensitive. 
+
+For each serie, the script downloads a zip file from TheTVDB.com and extracts it. This
+file contains information about each episode. The script will try to match the
+season number and the season episode retrieved from the file name to the information
+in the file downloaded from TheTVDB.com. When the correct episode has been found, the
+thumbnail image is saved in the path used as the input argument when starting the
+script.
+
+For each serie, the script has to contact TheTVDB.com to get the serie ID. Once found,
+the ID is stored in a database. First, the database is checked for the serie ID,
+before TheTVDB.com is contacted. This improves speed, and reduced the number of API call.
+
+The files downloaded from TheTVDB.com are saved. If the file already exists, the file is
+not downloaded again. This also improves speed and reduces the load on the TheTVDB.com
+server. If the file is more that 6 days old, it will always be retrieved again
+from TheTVDB.com, to make sure new episodes are included.
+
+### Movies
+
+If the script determines that a file is not a series, it tries to download the movie
+poster from TheMovieDB.org. Movies are also matched using regular expression. The following
+patterns are recognised:
+
+- The file name has a year in in, surrounded by (), [], or {}, or not surrounded
+- The file file name
+
+First, the name of the parent directory is tried. If no movie can be found, the file name
+itself is used. Both the directory and the file name use the same patterns.
+
+## Configuration settings
+
+A number of settings can be configured. Open the file `tw-video-scraper.py`, and
+change the settings on top of the page. Documentation is included above each
+setting.
+
+## Serie/Movie cannot be found OR Wrong serie/movie is retrieved
+
+If the serie/movie cannot be found, or the wrong serie/movie information is retrieved, there
+are some ways to deal with this. The starting point is the name of each video
+file. The script must be able to guess the name of the serie or movie from just the file name.
+This is done by regular expression. New regular expression patterns can be added by adjusting
+the settings.
+
+Sometimes the file name is in a different format, e.g. foo.bar.2011.s01e01.aaa.avi.
+This includes the year of the series. The script sees the name as 'foo bar 2011'
+and will try to match that. If this cannot be found, no thumbnail will be retrieved. The same
+goes for movies. A folder name or file name like 'foo.bar.hdtv.avi' cannot be matched.
+One option is to change the name of all the files. Another option is to make (mis)use
+of the database. Add a new line to the database with the serie name and the correct
+ID, and the script will then always use this ID.
+
+	$ sqlite3 tw-video-scraper.db
+	>> insert into video (id,type,name) values (TVDB_ID,'serie','foo bar 2011');
+or
+
+	>> insert into video (id,type,name) values (MOVIEDB_ID,'movie','foo bar');
+	>> .exit
+
+## Future developments
+
+If you can, please contribute by submitting bug/fixes/new code through GitHub.
+
+## Licence & legal stuff
+
+This code may be distributed and adapted under a BSD licence. See the file LICENSE for more
+information.
+
+This product is not associated with TwonkyMedia or PacketVideo in any way.
+TwonkyMedia server (c)2011 PacketVideo Corporation
+
+This product uses the TheTVDB.com API. You can help support TheTVDB.com
+by contributing information and artwork.
+
+This product uses the TMDb API but is not endorsed or certified by TMDb.
---
+++ tw-video-scraper.py
@@ -0,0 +1,466 @@
+settings = {
+	# path and name of the sqlite3 database to use
+	# use only a file name to use the database in
+	# the current working directory
+	# Leave empty to disable the database
+	'database': '/var/twonkymedia/twonkymedia/tw-video-scraper.db',
+	
+	# Path where to download and extract the files
+	# downloaded from TVDB. The script will check
+	# if a file already exists, so it will not be
+	# downloaded again.
+	'tmpdir': '/tmp/tw-video-scraper/',
+
+	# List of name of the parent directories
+	# Used for matching the directory name of the name
+	# of a movie/serie. By knowing the parent dir,the
+	# script knows if it should analyse the name
+	# E.g. ~/download/my.movie.2011.avi vs. ~/download/My.Movie.2011/part1.avi
+	'parentdir': ['download','series','movies'],
+	
+	# Regular expression patterns to determine if
+	# a file is a series. First match should product
+	# the title, second match the season, third match
+	# the episode number
+	'seriepatterns': ['(.*?)S(\d{1,2})E(\d{2})(.*)',
+					  '(.*?)\[?(\d{1,2})x?(\d{2})\]?(.*)',
+					  '(.*?)Season.?(\d{1,2}).*?Episode.?(\d{1,2})(.*)'],
+	
+	# TVDB api settings
+	'tvdbapikey': '6262A88CCAB7E724',
+	'tvdblang': 'en',
+	
+	# Regular expression patterns for movies
+	# First try to get the year from the title
+	# If that fails, try to match the whole name
+	'moviepatterns': ['(.*?)\((\d{4})\)(.*)',
+					  '(.*?)\[(\d{4})\](.*)',
+					  '(.*?)\{(\d{4})\}(.*)',
+					  '(.*?)(\d{4})(.*)',
+					  '(.*)'],
+	# MovieDB api settings
+	'moviedbapikey': 'a8b9f96dde091408a03cb4c78477bd14',
+
+	# Files downloaded from TVDB are stored in the tmpdir,
+	# and not downloaded if already present. If the file
+	# is too old, it will be deleted and downloaded again
+	# This is the number of days after which to delete files
+	# and download it again
+	'cacherenew': 6,
+	
+	# If no thumbnail can be found, a thumbnail can be generated
+	# using the following command. Leave empty to disable generating
+	# thumbnails
+	'generatecommand': 'ffmpeg -itsoffset -30 -i "$infile" -y -vcodec mjpeg -vframes 1 -an -f rawvideo -s 624x352 "$outfile"',
+}
+
+def main():
+	import sys
+	
+	if not Config['tmpdir'].endswith('/'):
+		Config['tmpdir'] = Config['tmpdir']+'/'
+	
+	inputfile = None
+	inputdirectory = None
+	outputimage = None
+	
+	# Validate input arguments
+	if len(sys.argv) < 3:
+		print "Usage: tw-video-scraper.py inputmovie outputimage"
+		exit()
+	
+	# for windows path names
+	if sys.argv[1].find('\\') >= 0:
+		sys.argv[1] = sys.argv[1].replace('\\','/')
+		
+	serie = Serie(sys.argv[1])
+	
+	if serie.isSerie():
+		thumbnail = serie.getThumbnail()
+	else:
+		movie = Movie(sys.argv[1])
+		thumbnail = movie.getThumbnail()
+
+	if thumbnail:
+		print 'Downloading file '+thumbnail+'...'
+		URL(thumbnail).download(sys.argv[2])
+	else:
+		file = sys.argv[1]
+		if file.find('/') >= 0:
+			file = file[file.rindex('/')+1:]
+			
+		print 'Could not get enough information from file name \''+file+'\''
+		if Config['generatecommand'] != '':
+			print 'Generating thumbnail...'
+			try:
+				import subprocess as sub
+				Config['generatecommand'] = Config['generatecommand'].replace('$infile',sys.argv[1]).replace('$outfile',sys.argv[2])
+				p = sub.Popen(Config['generatecommand'],stdout=sub.PIPE,stderr=sub.PIPE,shell=True)
+				output, errors = p.communicate()
+			except:
+				print 'Failed to execute generate thumbnail command'
+				
+	
+class Serie:
+	fileName = None
+	file = None
+	path = None
+	name = None
+	id = None
+	season = None
+	episode = None
+	thumbnail = None
+	
+	def __init__(self, fileName):
+		self.fileName = fileName
+		self._parseFileName()
+	
+	def isSerie(self):
+		if self.name and self.season and self.episode and self.id:
+			return True
+		else:
+			return False
+		
+	def getThumbnail(self):
+		if not self.thumbnail:
+			self._getTVDBThumbnail()
+		return self.thumbnail
+
+	def _parseFileName(self):
+		import re
+
+		match = None
+		i = 0	
+		patterns = Config['seriepatterns']
+		
+		# Separate the path name and the file name
+		if self.fileName.find('/') >= 0:
+			self.file = self.fileName[self.fileName.rindex('/')+1:]
+			self.path = self.fileName[0:self.fileName.rindex('/')+1]
+		else:
+			self.file = self.fileName
+			self.path = ''
+		
+		while not match:		
+			if i > len(patterns)-1:
+				return False
+			pattern = re.compile(patterns[i], re.IGNORECASE)
+			match = pattern.match(self.file)
+			i = i+1
+
+		self.name = match.group(1).replace('.',' ').replace('_',' ').lower().strip()
+		self.season = int(match.group(2))
+		self.episode = int(match.group(3))
+		
+		if self.name.endswith('-'):
+			self.name = self.name[0:len(self.name)-1].strip()
+		
+		if self._retrieveID():
+			return True
+		else:
+			return False
+	
+	def _retrieveID(self):
+		if db.isEnabled():
+			db.execute('SELECT id FROM video WHERE type=\'serie\' and name=\''+self.name+'\'')		
+			if db.rowcount() > 0:
+				self.id = str(db.fetchrow()[0])
+		
+		if not self.id:
+			apicall = URL('http://www.thetvdb.com/api/GetSeries.php?language=en&seriesname='+self.name).open()
+			if apicall:
+				from xml.dom import minidom
+				dom = minidom.parse(apicall)
+				for node in dom.getElementsByTagName('Series'):
+					if node.getElementsByTagName('SeriesName')[0].firstChild.nodeValue.lower().replace(':','').replace('/','') == self.name:
+						self.id = node.getElementsByTagName('seriesid')[0].firstChild.nodeValue
+				
+				if self.id and db.isEnabled():
+					db.execute('INSERT INTO video (id,type,name) VALUES ('+str(self.id)+',\'serie\',\''+self.name+'\')')
+			else:
+				print 'Could not connect to theTVDB.com server.'
+		
+		return self.id
+	
+	
+	def _getTVDBzipfile(self):
+		import os, time
+		if not os.path.isdir(Config['tmpdir']+self.id):
+			os.makedirs(Config['tmpdir']+self.id)
+		if os.path.isfile(Config['tmpdir']+self.id+'/'+Config['tvdblang']+'.xml'):
+			if os.path.getctime(Config['tmpdir']+self.id+'/'+Config['tvdblang']+'.xml') < time.time()-(Config['cacherenew']*86400):
+				os.remove(Config['tmpdir']+self.id+'/'+Config['tvdblang']+'.xml')
+				os.remove(Config['tmpdir']+self.id+'/banners.xml')
+				os.remove(Config['tmpdir']+self.id+'/actors.xml')
+				os.remove(Config['tmpdir']+self.id+'.zip')
+		if not os.path.isfile(Config['tmpdir']+self.id+'/'+Config['tvdblang']+'.xml'):
+			if URL('http://www.thetvdb.com/api/'+Config['tvdbapikey']+'/series/'+self.id+'/all/'+Config['tvdblang']+'.zip').download(Config['tmpdir']+self.id+'.zip'):
+				Zip(Config['tmpdir']+self.id+'.zip').extract(Config['tmpdir']+self.id)			
+				return True
+		else:
+			# zip and xml file are already downloaded, no need to download again	
+			return True
+
+		print 'Error retrieving/processing files from theTVDB.com'
+		return False
+	
+	def _getTVDBThumbnail(self):
+		from xml.dom import minidom
+		
+		if self.id:
+			if self._getTVDBzipfile():
+				dom = minidom.parse(Config['tmpdir']+self.id+'/'+Config['tvdblang']+'.xml')
+				for node in dom.getElementsByTagName('Episode'):
+					if int(node.getElementsByTagName('SeasonNumber')[0].firstChild.nodeValue) == self.season and int(node.getElementsByTagName('EpisodeNumber')[0].firstChild.nodeValue) == self.episode:			
+						if node.getElementsByTagName('filename')[0].firstChild:		
+							self.thumbnail =  'http://www.thetvdb.com/banners/'+node.getElementsByTagName('filename')[0].firstChild.nodeValue
+							return True
+		return False
+
+class Movie:
+	fileName = None
+	file = None
+	path = None
+	parentfolder = None
+	name = None
+	year = None
+	id = None
+	thumbnail = None
+	
+	def __init__(self, fileName):
+		self.fileName = fileName
+		self._parseFileName()
+	
+	def isMovie(self):
+		if self.name and self.id:
+			return True
+		else:
+			return False
+			
+	def getThumbnail(self):
+		return self.thumbnail
+		
+	def _getMovieDBThumbnail(self, name, year = None):
+		from xml.dom import minidom
+		
+		match = False
+		
+		if db.isEnabled():			
+			if year:
+				db.execute('SELECT id FROM video WHERE type=\'movie\' and name=\''+name+'\' and year='+year)
+			else:
+				db.execute('SELECT id FROM video WHERE type=\'movie\' and name=\''+name+'\'')	
+			if db.rowcount() > 0:
+				self.id = str(db.fetchrow()[0])
+				apicall = URL('http://api.themoviedb.org/2.1/Movie.getInfo/en/xml/'+Config['moviedbapikey']+'/'+self.id).open()
+		
+		if not self.id:
+			if year:
+				apicall = URL('http://api.themoviedb.org/2.1/Movie.search/en/xml/'+Config['moviedbapikey']+'/'+name+' '+year).open()
+			else:
+				apicall = URL('http://api.themoviedb.org/2.1/Movie.search/en/xml/'+Config['moviedbapikey']+'/'+name).open()
+		
+		if apicall:
+			dom = minidom.parse(apicall)
+			if int(dom.getElementsByTagName('opensearch:totalResults')[0].firstChild.nodeValue) == 1:
+				# If the search has only 1 result, presume that's the correct movie
+				match = True
+			
+			for node in dom.getElementsByTagName('movie'):
+				# try to match the name, and -if exist- the original name or alternative name
+				if self._cleanupName(node.getElementsByTagName('name')[0].firstChild.nodeValue) == name:
+					match = True
+				if node.getElementsByTagName('original_name') and node.getElementsByTagName('original_name')[0].firstChild:
+					if self._cleanupName(node.getElementsByTagName('original_name')[0].firstChild.nodeValue) == name:
+						match = True
+				if node.getElementsByTagName('alternative_name') and node.getElementsByTagName('alternative_name')[0].firstChild:
+					if self._cleanupName(node.getElementsByTagName('alternative_name')[0].firstChild.nodeValue) == name:
+						match = True
+				
+				if match:
+					
+					if db.isEnabled():
+						self.id = node.getElementsByTagName('id')[0].firstChild.nodeValue
+						if year:
+							db.execute('INSERT INTO video (id,type,name,year) VALUES ('+str(self.id)+',\'movie\',\''+name+'\','+year+')')
+						else:
+							db.execute('INSERT INTO video (id,type,name,year) VALUES ('+str(self.id)+',\'movie\',\''+name+'\')')	
+
+					if node.getElementsByTagName('images')[0]:
+						for node2 in node.getElementsByTagName('image'):
+							if node2.attributes['type'].nodeValue == 'poster' and node2.attributes['size'].nodeValue == 'cover':
+								self.thumbnail = node2.attributes['url'].nodeValue
+								return True
+				# Set to False, for the next run of the loop
+				match = False
+		
+		return False
+		
+	def _cleanupName(self, name):
+		name = name.lower()
+		name = name.replace(':','')
+		name = name.replace('/','')
+		return name
+
+	def _parseFileName(self):
+
+		match = None
+		i = 0	
+		patterns = Config['moviepatterns']
+		# Separate the path name and the file name
+		if self.fileName.find('/') >= 0:
+			self.file = self.fileName[self.fileName.rindex('/')+1:]
+			self.path = self.fileName[0:self.fileName.rindex('/')+1]
+			# remove the last /
+			self.parentfolder = self.path[0:len(self.path)-1]
+			self.parentfolder = self.parentfolder[self.parentfolder.rindex('/')+1:]
+		else:
+			self.file = self.fileName
+		
+		# If there is a parent folder, first try that match that
+		if self.parentfolder and Config['parentdir'].count(self.parentfolder) == 0:
+			if self._matchPattern(self.parentfolder):
+				if self._getMovieDBThumbnail(self.name, self.year):
+					return True	
+				
+		# If the script comes here, no thumbnail could be found based on the parent directory's name
+		# Next, try the file name
+		if self._matchPattern(self.file):
+			if self._getMovieDBThumbnail(self.name, self.year):
+				return True	
+			
+		return False
+		
+	def _matchPattern(self, matchPattern):
+		import re
+		
+		match = None
+		i = 0	
+		patterns = Config['moviepatterns']
+		
+		while not match:		
+			if i > len(patterns)-1:
+				return False
+			pattern = re.compile(patterns[i], re.IGNORECASE)
+			match = pattern.match(matchPattern)
+			i = i+1
+
+		self.name = match.group(1).replace('.',' ').replace('_',' ').lower().strip()
+		if len(match.groups()) > 1:
+			self.year = match.group(2).strip()
+	
+		if self.name:
+			return True
+			
+		return False
+		
+class Database:
+	_sql = None
+	_result = None
+	_enabled = False
+	_row = 0
+	
+	def __init__(self, database = None):
+		if database:
+			try:
+				import sqlite3
+			
+				self._sql = sqlite3.connect(database)
+			
+				self._result = self._sql.execute('create table if not exists video (id int, type text, name text, year int)')
+				self._sql.commit()
+				self._enabled = True
+			except:
+				print "Could not open/create database. Running without database..."
+		else:
+			print "Database disabled. Running without database..."
+
+	def isEnabled(self):
+		return self._enabled
+
+	def execute(self, query):
+		if not self._sql:
+			return False
+		try:
+			self._result = self._sql.execute(query)
+			self._sql.commit()
+			self._result = self._result.fetchall()
+			self._row = 0
+		except:
+			return False
+	
+	def close(self):
+		if not self._sql:
+			return False
+		self._sql.close()
+		
+	def rowcount(self):
+		if not self._sql:
+			return False
+		return len(self._result)
+	
+	def fetchrow(self):
+		if not self._sql:
+			return False
+		if len(self._result) <= self._row:
+			return None
+		t = self._result[self._row]
+		self._row = self._row+1
+		return t
+	
+	def fetchall(self):
+		if not self._sql:
+			return False
+		return self._result
+
+class URL:
+	url = None
+	
+	def __init__(self, url):
+		self.url = url
+	
+	def open(self):
+		import urllib
+		try:
+			return urllib.urlopen(self.url)
+		except:
+			return None
+	
+	def download(self, location):
+		import urllib
+		try:
+			if urllib.urlretrieve(self.url, location):
+				return True
+			else:
+				return False
+		except:
+			return False
+
+class Zip:
+	zipfile = None
+	
+	def __init__(self, zipfile):
+		self.zipfile = zipfile
+	
+	def extract(self, location):
+		import zipfile, os
+		try:
+			if not location.endswith('/'):
+				location = location+'/'
+			fh = open(self.zipfile, 'rb')
+			z = zipfile.ZipFile(fh)
+			for name in z.namelist():
+				if name.endswith('/'):
+					os.makedirs(location+name)
+				else:
+					outfile = open(location+name, 'wb')
+					outfile.write(z.read(name))
+					outfile.close()
+			fh.close()
+		except:
+			return
+
+Config = dict(settings)
+db = Database(Config['database'])
+main()
+db.close()
